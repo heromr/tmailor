4207bec21e32c058f8ab59e97520d9320acbe9f2
HeroMR <84679204+heromr@users.noreply.github.com>
2023-07-17 11:51:22 +0300

    Update __init__.py

--- tmailor/__init__.py
+++ tmailor/__init__.py
@@ -1,3 +1,17 @@
-from .client import (
-    Client
-)
+__title__ = 'tmailor'
+__author__ = 'heromr'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 HeroMR'
+__version__ = '0.0.12'
+__description__ = 'A temporary email address that provides email addresses without registration, used to receive incoming emails without disclosing your actual email.'
+
+
+from .client import Client
+
+from requests import get
+from colorama import Fore, Style
+
+latestVersion = get("https://pypi.org/pypi/tmailor/json").json()["info"]["version"]
+
+if __version__ != latestVersion:
+    print(f"{Fore.RED}WARNING:{Style.RESET_ALL} You are using an outdated version of tmailor ({__version__}). The latest version is {latestVersion}.")
